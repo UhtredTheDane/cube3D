d0d11e3c44da565f4e27d2f0a9f9a505c6a1ac85
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-08-17 15:42:45 +0200

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
+int main(int argc, char **argv)
+{
+  size_t size_name_map;
+  
+  if (argc != 2)
+  {
+    printf("Usage: ./cube3D YourMap.cub\n");
+    return (1);
+  }
+  size_name_map = ft_strlen(argv[1]);
+  if ()
+  
+}
