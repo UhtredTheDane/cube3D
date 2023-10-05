2e9768ba1efa3f4a9a2680ae09c4cf546103ad04
anmande <andremandevie@gmail.com>
2023-10-05 11:43:42 +0200

    algo fonctionne

--- srcs/player/player.c
+++ srcs/player/player.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/17 14:58:24 by agengemb          #+#    #+#             */
-/*   Updated: 2023/10/05 11:40:47 by anmande          ###   ########.fr       */
+/*   Created: 2023/10/05 11:37:20 by anmande           #+#    #+#             */
+/*   Updated: 2023/10/05 11:40:06 by anmande          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/player.h"
 
-#ifndef MAIN_H
-# define MAIN_H
-
-# include <stdio.h>
-# include "../libft/libft.h"
-# include "canvas.h"
-# include "window.h"
-# include "loading.h"
-# include "../mlx_linux/mlx.h"
-# include "player.h"
-#endif
