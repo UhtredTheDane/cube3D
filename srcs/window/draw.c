e76995bbeeef8c7534999cbba40a6e94b2bb4be1
agengemb <gengembre.alexis@gmail.com>
2023-10-05 13:17:06 +0200

    merge presque bon

--- srcs/window/draw.c
+++ srcs/window/draw.c
@@ -6,7 +6,7 @@
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:19:51 by anmande           #+#    #+#             */
-/*   Updated: 2023/10/05 11:26:35 by anmande          ###   ########.fr       */
+/*   Updated: 2023/10/05 13:15:41 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 void	my_mlx_pixel_put(t_canvas *canvas, int x, int y, int color)
 {
-	//char	*dst;
-	x = y + color;
-	(void) x;
-	(void) canvas;
-	/*dst = canvas->addr + (y * canvas->map->line_nb + x * (canvas->bpp / 8));
-	*(unsigned int *)dst = color;*/
+	char	*dst;
+
+	dst = canvas->addr + (y * canvas->map->line_nb + x * (canvas->bpp / 8));
+	*(unsigned int *)dst = color;
 }
 
 void	new_image(t_canvas *canvas)
@@ -60,11 +58,20 @@ void	draw_map(t_canvas *canvas)
 		while (j < canvas->map->row_nb)
 		{
 			if (canvas->map->block_map[i][j].type == '1')
+			{
+				printf("i: %ld et j: %ld rouge\n", i, j);
 				draw_squar(canvas, 0xFF0000, i, j);
+			}
 			else if (canvas->map->block_map[i][j].type == '0')
+			{
+				printf("i: %ld et j: %ld vert\n", i, j);
 				draw_squar(canvas, 0x00FF00, i, j);
+			}
 			else if (canvas->map->block_map[i][j].type == ' ')
+			{
+				printf("i: %ld et j: %ld bleu\n", i, j);
 				draw_squar(canvas, 0x0000FF, i, j);
+			}
 			j++;
 		}
 		i++;
--- srcs/window/window.c
+++ srcs/window/window.c
@@ -6,7 +6,7 @@
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:29:19 by anmande           #+#    #+#             */
-/*   Updated: 2023/10/05 11:19:43 by anmande          ###   ########.fr       */
+/*   Updated: 2023/10/05 12:43:49 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	create_map_2D(t_canvas *canvas, char *av)
 	canvas->fd = open(av, O_RDONLY);
 	if (canvas->fd == -1)
 		return (0);
-	draw_map(canvas, canvas->fd);
+	draw_map(canvas);
 	//draw_squar(win, 0x0000FF);
 	return (0);
 }
