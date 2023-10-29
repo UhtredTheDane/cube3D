0d3e272c3c70d38a7eae8b8d4c69622e8b65b88e
agengemb <gengembre.alexis@gmail.com>
2023-10-29 20:39:40 +0100

    test

--- includes/block.h
+++ includes/block.h
@@ -6,7 +6,7 @@
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:32:29 by anmande           #+#    #+#             */
-/*   Updated: 2023/10/27 16:32:29 by anmande          ###   ########.fr       */
+/*   Updated: 2023/10/29 20:38:26 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct s_block
 {
 	char	type;
 }			t_block;
-
+ 
 void	init_block(t_block *block, char symbol);
 void	free_block_map(t_block **block_map, size_t index);
 #endif
